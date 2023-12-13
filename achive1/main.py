4d6864b3e520f00306d1b6b894ffc8830d3f1ac4
Viktoria Mirzoyan <78561853+veekus@users.noreply.github.com>
2023-12-13 17:25:58 +0300

    Update main.py

--- achive1/main.py
+++ achive1/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import mysql.connector
+import logging
 
 
 app = Flask(__name__)
@@ -20,27 +21,40 @@ def checkIfNumberExists(cursor,number):
     result = cursor.fetchone()
     return result
 
-@app.route("/", methods=["POST"])
+@app.route('/', methods=['POST', 'GET'])
 def sendNumberRequest():
+    #logging.basicConfig(filename=f'/home/veekus/rvs/log.log', encoding='utf-8', level=logging.DEBUG)
+    #logging.info(f'The container was run at {datetime.now()}')
     connec = getDBConnection()
     cursor = connec.cursor()
     try:
-        number = request.json["number"]
-    except ValueError:
-        return jsonify({"error": "Пожалуйста, введите корректный номер."})
-    result = checkIfNumberExists(cursor,number)
-    result2 = checkIfNumberExists(cursor,int(number+1))
-    if result:
-        return jsonify({"error": "Номер уже был введен."})
-    if result2:
-            return jsonify({"error": "Номер до введен уже был введен."})
+        if request.method == 'POST':
+            number = int(request.form['number'])
+            #request.json["number"]
+            result = checkIfNumberExists(cursor,number)
+            result2 = checkIfNumberExists(cursor,int(number+1))
+            if result:
+                #logging.info(f'Номер уже был введен')
+                return jsonify({"error": "Номер уже был введен."})
+            if result2:
+                #logging.info(f'Номер до введен уже был введен')
+                return jsonify({"error": "Номер до введен уже был введен."})
 
-    connec.cursor().execute("INSERT INTO numbers (number) VALUES (%s)", (number,))
-    connec.commit()
+            connec.cursor().execute("INSERT INTO numbers (number) VALUES (%s)", (number,))
+            connec.commit()
 
-    next_number = number + 1
-    return jsonify({"result": next_number})
-
+            next_number = number + 1
+            return jsonify({"result": next_number})
+        else:
+            return """
+            <form method="post">
+                <label for="number">Введите число:</label>
+                <input type="number" id="number" name="number">
+                <input type="submit" value="Сохранить">
+            </form>
+            """
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 if __name__ == "__main__":
